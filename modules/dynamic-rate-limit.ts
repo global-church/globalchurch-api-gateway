95598ce8a2182146dbe98d0c3721a9f368e88092
Trent Sikute <tsikute@gmail.com>
2025-08-20 17:05:24 -0700

    Update dynamic-rate-limit.ts

--- modules/dynamic-rate-limit.ts
+++ modules/dynamic-rate-limit.ts
@@ -1,4 +1,6 @@
 // modules/dynamic-rate-limit.ts
+// currently not using this script due to the complexity, but may revist at a later date.
+
 import {
   CustomRateLimitDetails,
   ZuploRequest,
@@ -8,12 +10,17 @@ import {
 export function getLimitFromMetadata(
   request: ZuploRequest,
   context: ZuploContext
+  policyName: string,
 ): CustomRateLimitDetails {
   // The 'require-api-key' policy runs first, so the consumer
   // details are available on the context object.
-  const consumer = context.consumer;
-
-  if (consumer?.metadata?.rateLimit) {
+  const consumer = request.user;
+  context.log.info(
+    //if you can figure out how to properly call the consumer and the consumer's rate-limit policy from the metadata, this should work:
+    `processing consumer '${consumer.sub}' for rate-limit policy '${policyName}'`,    
+  );
+  
+  if (consumer.data.rateLimit) {
     // If the consumer has rate limit metadata, use it
     context.log.info(`Applying custom rate limit for consumer: ${consumer.subject}`);
     return {
